e5ce645170f5b6c60783cdd5fd61261d8eb57cd8
yorkyu(俞永真) <yorkyu@tencent.com>
2018-04-22 09:23:12 +0800

    完成拉取经纬度数据

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var http = require('http');
+var fs = require('fs');
 var city = require('./city.json');
 var urlencode = require('urlencode');
 
 async function curlGet(url) {
-	let json = await new Promise((resolve, rej) => {
+	let json = await new Promise((resolve, reject) => {
 		setTimeout(() => {
 			var options = {
 				hostname: 'apis.map.qq.com',
@@ -14,7 +15,7 @@ async function curlGet(url) {
 				    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
 				}
 			};
-			
+			console.log(`http://${options.hostname}${url}`);
 			let req = http.request(options, (res) => {
 				let size = 0;
     			let chunks = [];
@@ -24,10 +25,15 @@ async function curlGet(url) {
 				});
 				res.on('end', () => {
 					let data = Buffer.concat(chunks, size);
-					resolve(JSON.parse(data.toString()));
+					try {
+						resolve(JSON.parse(data.toString()));
+					} catch(e) {
+						reject(e);
+					}
 				})
 			});
 			req.on('error', (e) => {
+				reject(e);
 			});
 			req.end();
 		}, 1000);
@@ -39,53 +45,111 @@ async function generateLatLng(city) {
 	let format = [];
 	let url = '/ws/district/v1/search?&keyword=${key}&key=Y7GBZ-W6XKQ-5YF5L-GKI4K-VD2H2-GIF7O';
 	for (let i = 0; i < city.length; i++) {
-		let province = {};
+		let provinceName = city[i]['n'];	
+		let provinceObj = {
+			text: provinceName,
+			value: provinceName,
+		};
 		let sub = city[i]['s'];
 		if (sub) {
 			for (let j = 0; j < sub.length; j++) {
+				let cityName = sub[j]['n'];
+
 				let sub2 = sub[j]['s'];
 				if (sub2) {
 					for (let k = 0; k < sub2.length; k++) {
-						await curlGet(url.replace('${key}', urlencode(sub2[k]['n']))).then((data) => {
-							let realPos = getRealPos(data.result, city[i]['n']);
+						let regionName = sub2[k]['n'];
+
+						if (!provinceObj['sub']) {
+							provinceObj['sub'] = [];
+						}
+
+						if (!provinceObj['sub'][j]) {
+							provinceObj['sub'][j] = {};
+						}									
+						provinceObj['sub'][j]['text'] = cityName;
+						provinceObj['sub'][j]['value'] = cityName;
+
+						await curlGet(url.replace('${key}', urlencode(regionName))).then((data) => {
+							let result = data.result.length > 0 ? data.result[0] : [];
+							let realPos = getRealPos(result, provinceName, cityName);
+							if (realPos) {
+								if (!provinceObj['sub'][j]['sub']) {
+									provinceObj['sub'][j]['sub'] = [];
+								}
+								let regionItem = {
+									text: regionName,
+									value: regionName,
+									location: {
+										lat: realPos.location.lat,
+										lng: realPos.location.lng,
+									}	
+								} 
+								provinceObj['sub'][j]['sub'].push(regionItem);
+							}
 						}).catch((error) => {
+
 						});	
 					}
 				} else {
-					await curlGet(url.replace('${key}', urlencode(sub[j]['n']))).then((data) => {
-						let realPos = getRealPos(data.result, city[i]['n']);
+					await curlGet(url.replace('${key}', urlencode(cityName))).then((data) => {
+						let result = data.result.length > 0 ? data.result[0] : [];
+						let realPos = getRealPos(result, provinceName, cityName);
+						if (realPos) {
+							if (!provinceObj['sub']) {
+								provinceObj['sub'] = [];
+							}
+							let cityItem = {
+								text: cityName,
+								value: cityName,
+								location: {
+									lat: realPos.location.lat,
+									lng: realPos.location.lng,
+								}	
+							} 
+							provinceObj['sub'].push(cityItem);
+						}
 					}).catch((error) => {
+
 					});	
 				}
 			}
 		} else {
-				await curlGet(url.replace('${key}', urlencode(city[i]['n']))).then((data) => {
-					let realPos = getRealPos(data.result, city[i]['n']);
+				await curlGet(url.replace('${key}', urlencode(provinceName))).then((data) => {
+					let result = data.result.length > 0 ? data.result[0] : [];
+					let realPos = getRealPos(result, provinceName, '');
+					if (realPos) {
+						provinceObj['location'] = {
+							lat: realPos.location.lat,
+							lng: realPos.location.lng,
+						};
+					}
 				}).catch((error) => {
+
 				});	
 		}
-		format.push(province);
+		format.push(provinceObj);
 	}
+	let formatStr = JSON.stringify(format);
+	fs.writeFileSync('./city-location.json', formatStr, {
+		encoding: 'utf8',
+	});
 }
 
-const getRealPos = (results, province) => {
-	console.log(results.constructor == Array);
-	console.log(results.length);
+const getRealPos = (results, provinceName, cityName) => {
 	let realPos = null;
-	if (results.length === 0) {
-		return null;
-	} else if (results.length === 1) {
-		return results[0];
-	} else {
-		province = province.replace(/(市|区|省|县)/, '');
-		let regx = new RegExp('^'+province);
-		for (let i = 0; i < results.length; i++) {
-			if (regx.test(results[i].address)) {
-				realPos = Object.assign({}, results[i]);
-			}	
-		}
-	}
-	console.log(realPos);
+
+	provinceName = provinceName.replace(/(市|区|省|县)$/, '');
+	cityName = cityName.replace(/(市|区|省|县)$/, '');
+	let regxProvince = new RegExp('^'+provinceName);
+	let regxCity = new RegExp('^' + cityName);
+	
+	results.forEach((element, index) => {		
+		let address = element.address.split(',');
+		if (regxProvince.test(address[0]) && regxCity.test(address[1])) {
+			realPos = Object.assign({}, element);
+		}	
+	});
 	return realPos;
 }
 
